1f1f597bb49b0ade942d6dc401577c811a696bb2
Stephanine Mow <stephaniemow@gmail.com>
2021-07-14 19:37:19 +0000

    Updated subtract, multiply and square

--- arithmetic.py
+++ arithmetic.py
@@ -3,25 +3,23 @@
 
 def add(num1, num2):
     """Return the sum of the two inputs."""
-
     return num1 + num2
-
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
-
+    return num1-num2
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
-
+    return num1*num2
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
-
+    return num1/num2
 
 def square(num1):
     """Return the square of the input."""
-
+    return num1**2
 
 def cube(num1):
     """Return the cube of the input."""
